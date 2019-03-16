51123080311cd7c8f125e3f5546dc3b634c519cf
Federico Camerota <fedes770@gmail.com>
2019-03-16 11:07:32 +0100

    Added complexity test

--- 02_chain_matrix_multiplication/complexity_test.c
+++ 02_chain_matrix_multiplication/complexity_test.c
@@ -1,40 +1,49 @@
 #include "../01_strassen/matrix.h"
 #include "chain_matrix_multiplication.h"
 #include <stdio.h>
+#include <time.h>
+
+double get_execution_time(const struct timespec b_time,
+                          const struct timespec e_time);
 
 int main(){
 
-    size_t n_max = 64;
+    size_t k = 12;
+    size_t n_max = (1U << k);
     int p[n_max+1];
     for (size_t i = 0; i < n_max; ++i){
     
-	p[i] = RANDOM_NUM;
+	p[i] = rand() % 40;
     }
     float **m = improved_allocate_matrix(n_max,n_max);
     float **s = improved_allocate_matrix(n_max, n_max);
 
-    
-    matrix_chain (p, n, m, s);
-    
-    printf("M matrix:\n");
-    for (size_t i = 0; i < n; ++i){
-    
-	for (size_t j = 0; j < n; ++j){
-	
-	   printf("%.0f\t", m[i][j]); 
-	}
-	putchar('\n');
+    double elapsed[k];
+    struct timespec b_time, e_time;
+
+    for (size_t n = 2, k_i = 0; n <= n_max; n *= 2, ++k_i){
+
+	clock_gettime(CLOCK_REALTIME, &b_time);
+	matrix_chain (p, n, m, s);
+	clock_gettime(CLOCK_REALTIME, &e_time);
+	elapsed[k_i] = get_execution_time(b_time, e_time);
+	printf("%lu: %lf\n", k_i, elapsed[k_i]);
     }
-    printf("S matrix:\n");
-    for (size_t i = 0; i < n - 1; ++i){
+
+    for (size_t k_i = 1; k_i < k; ++k_i){
     
-	for (size_t j = 0; j < n - 1; ++j){
-	
-	   printf("%.0f\t", s[i][j]); 
-	}
-	putchar('\n');
+        printf("%f\n", elapsed[k_i]/elapsed[k_i - 1]);
     }
+    
 
     improved_deallocate_matrix(m);
     improved_deallocate_matrix(s);
+}
+
+
+double get_execution_time(const struct timespec b_time,
+                          const struct timespec e_time)
+{
+  return (e_time.tv_sec-b_time.tv_sec) +
+                   (e_time.tv_nsec-b_time.tv_nsec)/1E9;
 }
