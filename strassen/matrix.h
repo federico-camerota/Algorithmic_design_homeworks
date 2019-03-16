b34e7f6d37d1ae3cd192bb2e4f195b69182b9280
Federico Camerota <fedes770@gmail.com>
2019-03-16 08:33:40 +0100

    Improved organization of code

--- strassen/matrix.h
+++ strassen/matrix.h
@@ -19,5 +19,25 @@ void improved_deallocate_matrix(float **A);
 
 int same_matrix(float **A, const size_t A_rows, const size_t A_cols,
 		float **B, const size_t B_rows, const size_t B_cols);
+void sum_matrix_blocks(float **C,
+                       const size_t C_f_row,
+                       const size_t C_f_col,
+                       float ** A,
+                       const size_t A_f_row,
+                       const size_t A_f_col,
+                       float ** B,
+                       const size_t B_f_row,
+                       const size_t B_f_col,
+                       const size_t n);
+void sub_matrix_blocks(float **C,
+                       const size_t C_f_row,
+                       const size_t C_f_col,
+                       float ** A,
+                       const size_t A_f_row,
+                       const size_t A_f_col,
+                       float ** B,
+                       const size_t B_f_row,
+                       const size_t B_f_col,
+                       const size_t n);
 
 #endif // __MATRIX__
--- strassen/strassen.c
+++ strassen/strassen.c
@@ -1,50 +1,10 @@
 #include <stdlib.h>
 #include "matrix.h"
 
-void sum_matrix_blocks(float **C,
-                       const size_t C_f_row,
-                       const size_t C_f_col,
-                       float ** A,
-                       const size_t A_f_row,
-                       const size_t A_f_col,
-                       float ** B,
-                       const size_t B_f_row,
-                       const size_t B_f_col,
-                       const size_t n)
-{
-  // for all the rows in the blocks
-  for (size_t i=0; i<n; i++) {
 
-    // for all the cols in the blocks
-    for (size_t j=0; j<n; j++) {
-      C[C_f_row+i][C_f_col+j] = A[A_f_row+i][A_f_col+j] +
-                                B[B_f_row+i][B_f_col+j];
-    }
-  }
-}
-
-void sub_matrix_blocks(float **C,
-                       const size_t C_f_row,
-                       const size_t C_f_col,
-                       float ** A,
-                       const size_t A_f_row,
-                       const size_t A_f_col,
-                       float ** B,
-                       const size_t B_f_row,
-                       const size_t B_f_col,
-                       const size_t n)
-{
-  // for all the rows in the blocks
-  for (size_t i=0; i<n; i++) {
-
-    // for all the cols in the blocks
-    for (size_t j=0; j<n; j++) {
-      C[C_f_row+i][C_f_col+j] = A[A_f_row+i][A_f_col+j] -
-                                B[B_f_row+i][B_f_col+j];
-    }
-  }
-}
-
+/*
+ * Auxiliary function for the naive implementation of the matrix multiplication algorithm
+ */
 void naive_aux(float **C, const size_t C_f_row, const size_t C_f_col,
                float **A, const size_t A_f_row, const size_t A_f_col,
                float **B, const size_t B_f_row, const size_t B_f_col,
@@ -62,6 +22,9 @@ void naive_aux(float **C, const size_t C_f_row, const size_t C_f_col,
    }
 }
 
+/*
+ * Auxiliary function for the Strassen's algorithm
+ */
 void strassen_aux(float **C, const size_t C_f_row, const size_t C_f_col,
                float **A, const size_t A_f_row, const size_t A_f_col,
                float **B, const size_t B_f_row, const size_t B_f_col,
@@ -256,12 +219,18 @@ void strassen_aux(float **C, const size_t C_f_row, const size_t C_f_col,
   }
 }
 
+/*
+ * Recursive implementation of the Strassen's algorithm
+ */
 void strassen(float **C,
       float **A, float **B, const size_t n)
 {
   strassen_aux(C, 0, 0, A, 0, 0, B, 0, 0, n);
 }
 
+/*
+ * Auxiliary function for the improved version of the Strassen's algorithm
+ */
 void improved_strassen_aux(float **C, const size_t C_f_row, const size_t C_f_col,
                float **A, const size_t A_f_row, const size_t A_f_col,
                float **B, const size_t B_f_row, const size_t B_f_col,
@@ -466,6 +435,9 @@ void improved_strassen_aux(float **C, const size_t C_f_row, const size_t C_f_col
  improved_deallocate_matrix(aux_matrix);
 }
 
+/*
+ * Improved implementation of the Strassen's algorithm
+ */
 void improved_strassen(float **C,
       float **A, float **B, const size_t n)
 {
