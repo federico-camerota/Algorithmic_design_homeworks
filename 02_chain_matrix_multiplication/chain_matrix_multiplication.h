67e3a0e1ae418be80d7c42f61a3517bafc994b69
Federico Camerota <fedes770@gmail.com>
2019-05-24 10:39:46 +0200

    Added comments

--- 02_chain_matrix_multiplication/chain_matrix_multiplication.h
+++ 02_chain_matrix_multiplication/chain_matrix_multiplication.h
@@ -3,6 +3,15 @@
 
 #include <stdlib.h>
 
+/*
+ * Computes the M and S matrices in the chain matrix multiplication algorithm.
+ * - p is the vector of matrix dimensions
+ * - n is the number of matrices in the chain
+ * - m is the matrix in which the M matrix will be stored
+ * - s is the matrix in which the S matrix will be stored
+ *
+ * Both m and s are asssumed to be n x n matrices.
+ */
 void matrix_chain (size_t *p, size_t n, size_t **m, size_t **s);
 
 #endif
--- 05_graphs/dijkstra.h
+++ 05_graphs/dijkstra.h
@@ -3,6 +3,20 @@
 
 #include <stdlib.h> 
 
+/*
+ * This struct represents a node in the graph
+ * - the id member is the number associated with each node
+ * - the pos member is the position of the node in the queue
+ *   used in the dijskra algorithm (for the array based version
+ *   this is equal to the id)
+ * - the d member is the distance from the source node
+ * - the prev member is a pointer to the previous node
+ *   in the shortest path from the source to this node.
+ *
+ *   A node that cannot be reached from the source node will 
+ *   have distance set to DBL_MAX and previous node set to
+ *   NULL.
+ */
 struct g_node {
     size_t id;
     size_t pos;
@@ -11,6 +25,15 @@ struct g_node {
 };
 typedef struct g_node graph_node;
 
+/*
+ * Array and heap based versions of the dijksra's algorithm
+ * Both versions return an array of graph_nodes representing all the nodes
+ * in the graph.
+ *
+ * adj_matrix is the adjacency matrix describing the graph
+ * n is the number of nodes in the graph
+ * source is the id of the source node from which shortest paths are to be computed
+ */
 graph_node *array_dijkstra(double **adj_matrix, const size_t n, const size_t source );
 graph_node  *heap_dijkstra(double **adj_matrix, const size_t n, const size_t source );
 
